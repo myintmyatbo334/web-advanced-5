98eabc9e2fd3cf6e18c0837ccedaf8a875713408
myintmyatbo334 <mmb281299@gmail.com>
2023-02-19 15:03:47 +0630

    upload js test file

---
+++ js-jquery/main.js
@@ -0,0 +1,10 @@
+// var cars = "Toyota";
+// var cars = "Honda";
+
+var x = '10';
+var y = '5';
+
+var cars = ["Honda", "Toyota", "Pro Box"];
+cars[3] = "Nissan";
+
+var people = ["HML", "WKP", "MMB", "Tom"];
